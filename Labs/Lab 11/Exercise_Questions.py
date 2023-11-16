32265f077c7aa2583588c8960600367fdb81d3f5
Emily Kaiser <emka6809@colorado.edu>
2023-11-16 10:52:11 -0700

    plots and code for lab 11

---
+++ Labs/Lab 11/Exercise_Questions.py
@@ -0,0 +1,7 @@
+# Exercise Question Answers
+
+# Based on the plots, the Gaussian quadrature is the best approximation. It reaches the desired accuracy in the first interval. 
+
+# Composite Simpson's doesn't reach the desired accuracy when n = 5.
+
+# Composite Trapezoidal reaches the desired accuracy in the second interval. 
